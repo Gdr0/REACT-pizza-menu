74a80af23a1177cad6a2ede23120ab9031c79335
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-06-26 17:48:22 +0200

    soldout

--- src/index.js
+++ src/index.js
@@ -56,6 +56,7 @@ function App() {
   );
   function Menu() {
     const pizzas = pizzaData;
+
     const numPizzas = pizzas.length;
     return (
       <main className="menu">
@@ -68,11 +69,17 @@ function App() {
           </ul>
         )} */}
         {numPizzas > 0 ? (
-          <ul className="pizzas">
-            {pizzas.map((pizza) => (
-              <Pizza pizzaObj={pizza} key={pizza.name} />
-            ))}
-          </ul>
+          <>
+            <p>
+              Authentic italian cousine. 6 creative dishes to choose from. All
+              from our stone oven, all organic, all delicious.
+            </p>
+            <ul className="pizzas">
+              {pizzas.map((pizza) => (
+                <Pizza pizzaObj={pizza} key={pizza.name} />
+              ))}
+            </ul>
+          </>
         ) : (
           <p>We're still working on our menu. Please come back later</p>
         )}
@@ -80,17 +87,22 @@ function App() {
     );
   }
 }
-function Pizza(props) {
-  console.log(props);
-  if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <div className="pizza">
-      <img src={props.pizzaObj.photoName} alt=""></img>
+    <div className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObj.photoName} alt=""></img>
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>props.pizzaObj.price</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        {/* {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )} */}
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </div>
   );
@@ -117,7 +129,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're closed. Business is open between {openHour}:00 and {closeHour}
@@ -128,12 +140,10 @@ function Footer() {
   );
   // return React.createElement("footer", null, "'we're chilling");
 }
-function Order(props) {
+function Order({ closeHour, openHour }) {
   return (
     <div className="order">
-      <p>
-        We're open until {props.closeHour}:00. Come visit us or order online.
-      </p>
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
       <button className="btn">ORDER</button>
     </div>
   );
